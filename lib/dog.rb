420205d5677df496a6294e08e405e20bcc55a623
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-10 21:13:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,7 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0] #pryed to findout what dog data was made of.
-      dog = Dog.new(name: dog_data[1],breed: dog_data[2],id: dog_data[0])
+      dog = Dog.new(name: dog_data[1],breed: dog_data[2],id: dog_data[0]) #look at initialize when setting a new instance.
     else
       dog = self.create(name: name, breed: breed)
     end
